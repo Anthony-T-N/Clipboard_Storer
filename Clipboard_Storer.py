e9e1c3a7d1af3415817915b4e44dd422a1a93888
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2019-09-05 15:21:09 +0930

    Script can now validate changes to the clipboard.

--- Clipboard_Storer.py
+++ Clipboard_Storer.py
@@ -1,1 +1,36 @@
+""" Clipboard_Storer.
 
+A simple python script that records all items to the clipboard and stores as text file.
+
+"""
+
+__author__ = 'Anthony T Nguyen'
+__version__ = '---'
+__date__ = '16.07.2019'
+
+import win32clipboard
+import time
+
+def options():
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardText('')
+    win32clipboard.CloseClipboard()
+    current_value = ('')
+    while True:
+        win32clipboard.OpenClipboard()
+        clipboard_value = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
+        if (clipboard_value != current_value):
+            current_value = clipboard_value
+            print("Completed")
+        time.sleep(0.1)
+
+def main():
+    """
+    Begin
+    """
+    options()
+
+if __name__ == '__main__':
+    main()
