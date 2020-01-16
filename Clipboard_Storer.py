3c17c0b76ba8467207a4d20ae71face70fb0f2ef
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-01-16 23:49:27 +1030

    Update Clipboard_Storer.py

--- Clipboard_Storer.py
+++ Clipboard_Storer.py
@@ -5,7 +5,7 @@ A simple python script that records all items to the clipboard and stores as tex
 """
 
 __author__ = 'Anthony T Nguyen'
-__version__ = '---'
+__version__ = '1.0.0'
 __date__ = '05.09.2019'
 
 import win32clipboard
